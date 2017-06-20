76314dbb247e0b41b0284ecb726b164008a8a619
楊德倫（Te-Lun Yang） <telunyang@gmail.com>
2017-06-20 14:49:44 +0800

    add UnitTest

---
+++ models/TestMaze.php
@@ -0,0 +1,42 @@
+<?php
+namespace models;
+
+use models\Maze;
+use models\TestMaze;
+
+class TestMaze
+{
+	private $obj;
+	
+	public function __construct()
+	{
+		try
+		{
+			$this->obj = new Maze();
+			$queue = $this->obj->move();
+			print_r($queue);
+			echo "finished";
+		}
+		catch(Exception $e)
+		{
+			print_r($e);
+		}
+	}
+	
+	public function __destruct(){}
+	
+	public function test()
+	{
+		try
+		{
+			$queue = $this->obj->move();
+			print_r($queue);
+			echo "finished";
+		}
+		catch(Exception $e)
+		{
+			print_r($e);
+		}
+		
+	}
+}
