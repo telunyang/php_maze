a41bd27de91ba0ee76ec7bcafc90633f776d7a71
楊德倫（Te-Lun Yang） <telunyang@gmail.com>
2017-06-21 14:37:17 +0800

    add check code which remove paths we moved in

--- models/Maze.php
+++ models/Maze.php
@@ -4,6 +4,7 @@ class Maze
     protected $map_data; //地圖
     protected $stack_path_record; //記錄每一次出發的路徑資料
     protected $arr_barrier; //路障，代表不可通行
+    protected $arr_remove; //因為走過、離圖或其它因素，在退一步後，被記錄下來，不再走冤枉路
     protected $arr_result; //儲存每一次走到終點的路徑資料，皆代表完成
     protected $begin_row, $begin_col, $end_row, $end_col; //開始 row,col 跟終點 row,col
     
@@ -36,6 +37,9 @@ class Maze
 	    		'row' => $this->begin_row,
 	    		'col' => $this->begin_col
 	    ];
+	    
+	    //因為走過、離圖或其它因素，在退一步後，被記錄下來的路徑，不用再走冤枉路
+	    $this->arr_remove = [];
 	    
 	    //設定路障範圍
 	    $this->arr_barrier = [
@@ -77,34 +81,50 @@ class Maze
 		    //判斷是否需要退回上一步
 		    $flag = false;
 		    
+		    //暫存當前位置用
+		    $row = 0;
+		    $col = 0;
+		    
 		    //計算前進的位置，並加以記錄。count 減 1 是為了抓最後的元素
 		    switch($direction)
 		    {
 		    	case 1:
+		    		$row = $this->stack_path_record[count($this->stack_path_record)-1]['row'] - 1;
+		    		$col = $this->stack_path_record[count($this->stack_path_record)-1]['col'];
+		    		
 		    	    $this->stack_path_record[] = [
-		    	         'row' => ($this->stack_path_record[count($this->stack_path_record)-1]['row'] - 1),
-		    	         'col' => $this->stack_path_record[count($this->stack_path_record)-1]['col']
+		    	    		'row' => ($this->stack_path_record[count($this->stack_path_record)-1]['row'] - 1),
+		    	         	'col' => $this->stack_path_record[count($this->stack_path_record)-1]['col']
 		    	    ];
 		    	break;
 		    		
 		    	case 2:
+		    		$row = $this->stack_path_record[count($this->stack_path_record)-1]['row'] + 1;
+		    		$col = $this->stack_path_record[count($this->stack_path_record)-1]['col'];
+		    		
 		    	    $this->stack_path_record[] = [
-		    	         'row' => ($this->stack_path_record[count($this->stack_path_record)-1]['row'] + 1),
-		    	         'col' => $this->stack_path_record[count($this->stack_path_record)-1]['col']
+		    	         	'row' => ($this->stack_path_record[count($this->stack_path_record)-1]['row'] + 1),
+		    	         	'col' => $this->stack_path_record[count($this->stack_path_record)-1]['col']
 		    	    ];
 		    	break;
 		    		
 		    	case 3:
+		    		$row = $this->stack_path_record[count($this->stack_path_record)-1]['row'];
+		    		$col = $this->stack_path_record[count($this->stack_path_record)-1]['col'] - 1;
+		    		
 		    	    $this->stack_path_record[] = [
-		    	          'row' => $this->stack_path_record[count($this->stack_path_record)-1]['row'],
-		    	          'col' => ($this->stack_path_record[count($this->stack_path_record)-1]['col'] - 1)
+		    	          	'row' => $this->stack_path_record[count($this->stack_path_record)-1]['row'],
+		    	          	'col' => ($this->stack_path_record[count($this->stack_path_record)-1]['col'] - 1)
 		    	    ];
 		    	break;
 		    		
 		    	case 4:
+		    		$row = $this->stack_path_record[count($this->stack_path_record)-1]['row'];
+		    		$col = $this->stack_path_record[count($this->stack_path_record)-1]['col'] + 1;
+		    		
 		    	    $this->stack_path_record[] = [
-		    	         'row' => $this->stack_path_record[count($this->stack_path_record)-1]['row'],
-		    	         'col' => ($this->stack_path_record[count($this->stack_path_record)-1]['col'] + 1)
+		    	       	 	'row' => $this->stack_path_record[count($this->stack_path_record)-1]['row'],
+		    	         	'col' => ($this->stack_path_record[count($this->stack_path_record)-1]['col'] + 1)
 		    	    ];
 		    	break;
 		    		
@@ -112,11 +132,17 @@ class Maze
 		    		//$this->move();
 		    	break;
 		    }
+		    
+		    //將走過的路記錄下來
+		    $this->arr_remove[] = [
+		    		'row' => $row,
+		    		'col' => $col
+		    ];
 
 		    /*
 		     * 若是移動後的位置，遇到下列條件，則退回上一步
 		     * 1. 超過地圖
-		     * 2. 走過的路 (暫不考慮)
+		     * 2. 走過的路
 		     * 3. 遇到障礙
 		     */
 		    
@@ -130,36 +156,34 @@ class Maze
 		    }
 		    else
 		    {
-		        // 2.走過的路
-// 		        for($j = 0; $j < (count($this->stack_path_record)-1); $j++)
-// 		        {
-// 		            if( $this->stack_path_record[$j]['row'] == $this->stack_path_record[count($this->stack_path_record)-1]['row'] && 
-// 		                $this->stack_path_record[$j]['col'] == $this->stack_path_record[count($this->stack_path_record)-1]['col'])
-// 		            {
-// 		                $flag = true;
-// 		                break;
-// 		            }
-// 		        }
+		    	// 2.走過的路
+		    	for($j = 0; $j < (count($this->arr_remove)-1); $j++)
+		    	{
+		    		if( $this->arr_remove[$j]['row'] == $this->stack_path_record[count($this->stack_path_record)-1]['row'] &&
+		    			$this->arr_remove[$j]['col'] == $this->stack_path_record[count($this->stack_path_record)-1]['col'])
+		    		{
+		    			$flag = true;
+		    			break;
+		    		}
+		    	}
 
 		        // 3.遇到障礙
-		    	if($flag == false)
-		    	{
-		    	    for($i = 0; $i < count($this->arr_barrier); $i++)
-		    	    {
-		    	        if( $this->arr_barrier[$i]['row'] == $this->stack_path_record[count($this->stack_path_record)-1]['row'] && 
-		    	            $this->arr_barrier[$i]['col'] == $this->stack_path_record[count($this->stack_path_record)-1]['col'] )
-		    	        {
-		    	            $flag = true;
-		    	            break;
-		    	        }
-		    	    }
-		    	}
+	    	    for($i = 0; $i < count($this->arr_barrier); $i++)
+	    	    {
+	    	        if( $this->arr_barrier[$i]['row'] == $this->stack_path_record[count($this->stack_path_record)-1]['row'] && 
+	    	            $this->arr_barrier[$i]['col'] == $this->stack_path_record[count($this->stack_path_record)-1]['col'] )
+	    	        {
+	    	            $flag = true;
+	    	            break;
+	    	        }
+	    	    }
 		    }
 
-		    //如果符合退回上一步的條件，則將目前走到的位置移除
+		    //如果符合退回上一步的條件，則將目前走到的位置移除，同時將移除位置加以記錄
 		    if( $flag == true )
 		    {
 		    	$item = array_pop($this->stack_path_record);
+		    	//$this->arr_remove[] = $item;
 		    }
 		    
 		    //如果到達終點，則結束程式
